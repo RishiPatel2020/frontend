2535d358b790dc9fe660423daca80919fa47db5c
RishiPatel0290 <98559879+RishiPatel2020@users.noreply.github.com>
2024-04-20 22:06:32 -0400

    faq updated

--- src/Pages/Home/Questions/Question.js
+++ src/Pages/Home/Questions/Question.js
@@ -9,97 +9,95 @@ const Question = () => {
       </h2>
       <Accordion>
         <Accordion.Item eventKey="0">
-          <Accordion.Header>When do I have to order by?</Accordion.Header>
+          <Accordion.Header>
+            So there's no liking, swiping, or scrolling at all?
+          </Accordion.Header>
           <Accordion.Body>
-            Customers need to place their orders for meals at least 12 pm the
-            day before their intended day of delivery or else they will have to
-            choose to get it delivered in the next week.
+            None. Zilch. Zero. Nil. Hard to believe, but true. We believe in not
+            wasting time with having to manually go through tons of profiles to
+            check whether or not they fit your requirements. So, our systems do
+            the hard work behind the scenes to bring you profiles of folks that
+            match you.
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
-          <Accordion.Header>When do you deliver?</Accordion.Header>
-          <Accordion.Body>
-            We deliver all your meals for the week depending on Sundays.
-            Deliveries happen between 1pm - 2:30pm EST.
-          </Accordion.Body>
+          <Accordion.Header>
+            Is this a free matchmaking service?
+          </Accordion.Header>
+          <Accordion.Body>Yup. There's no charge. At all.</Accordion.Body>
         </Accordion.Item>
 
         <Accordion.Item eventKey="2">
           <Accordion.Header>
-            How many people can eat from each meal?
+            How many South Asian communities are included?
           </Accordion.Header>
           <Accordion.Body>
-            Each Mirchi Meal is designed to serve 1 adult. If you're ordering
-            for more people, we encourage you to follow our larger meal plans
-            (6, 8+ meals/week).
+            As of right now, we're covering alot of the bases but if we're
+            missing any particularities, we'll be adding them in soon.
           </Accordion.Body>
         </Accordion.Item>
 
         <Accordion.Item eventKey="3">
           <Accordion.Header>
-            Why can't I order less than 2 meals?
+            When can I expect to receive the e-mails of the matches?
           </Accordion.Header>
           <Accordion.Body>
-            We understand our customers are busy and sometimes chaotically busy.
-            As a result, we'd rather have our customers receive all their meals
-            for the week in one swoop than them stressing about single off
-            deliveries and ordering.
+            We send out the e-mails at the end of each month. Be on the lookout!
           </Accordion.Body>
         </Accordion.Item>
 
         <Accordion.Item eventKey="4">
           <Accordion.Header>
-            Can I just heat the container the meals come in?
+            Once I get the e-mail of the candidates, what's next?
           </Accordion.Header>
           <Accordion.Body>
-            Yes, of course! Our meals are intended to be brought out of the
-            fridge and reheated. No need to waste any cutlery, bowls, or dishes.
-            Remove the lid and place into the microwave.
+            You'll be able to send interests for any of the candidates in the
+            profiles and if they also do the same, you'll be invited to chat
+            with each other! Make sure you also submit the profiles you're not
+            interested in so that they don't roll over into the next cycle!
           </Accordion.Body>
         </Accordion.Item>
 
         <Accordion.Item eventKey="5">
-          <Accordion.Header>How do I preserve meals?</Accordion.Header>
+          <Accordion.Header>
+            How do I message the candidates in the profiles?
+          </Accordion.Header>
           <Accordion.Body>
-            We recommend that you place your meals you intend to consume
-            throughout the week in the fridge as soon you get your delivery.
+            As soon as the candidate also sends an interest to connect with you,
+            you'll be able to message one another!
           </Accordion.Body>
         </Accordion.Item>
 
         <Accordion.Item eventKey="6">
           <Accordion.Header>
-            How do I partner with Mirchi Meals?
+            Do I get the same profiles each month?
           </Accordion.Header>
           <Accordion.Body>
-            Mirchi Meals is always considering new partners to help fuel our fan
-            base throughout the week, shoot us an email at
-            support@mirchimeals.com
+            Unless you've not selected interested or not interested on the
+            profiles sent for a particular month, we won't send reoccurring
+            profiles as an excuse to not work harder for you. The systems at Ria
+            work around the clock to measure your profile with others as soon as
+            they enter our systems.
           </Accordion.Body>
         </Accordion.Item>
 
         <Accordion.Item eventKey="7">
-          <Accordion.Header>How do you ensure quality?</Accordion.Header>
+          <Accordion.Header>How are users verified?</Accordion.Header>
           <Accordion.Body>
-            Silly goose! Each customer is as equal as our family member. We hand
-            pick the best foods after tasting them, to ensure we can provide the
-            best quality to you.
+            As soon as the candidate also sends an interest to connect with you,
+            you'll be able to message one another!
           </Accordion.Body>
         </Accordion.Item>
 
         <Accordion.Item eventKey="8">
           <Accordion.Header>
-            Why do your meals remind me of home?
+            Did I miss the monthly e-mail of the matches?
           </Accordion.Header>
-          <Accordion.Body>It's just what we do best.</Accordion.Body>
-        </Accordion.Item>
-
-        <Accordion.Item eventKey="9">
-          <Accordion.Header>How can I cancel my order?</Accordion.Header>
           <Accordion.Body>
-            To cancel your order, write us an email at support@mirchimeals.com
-            for a cancellation of your order. You can cancel up till 3pm the day
-            prior to the selected delivery date. Refunds will be processed
-            within 7-10 business days.
+            If there's no profiles that match your preferences in our system for
+            a particular month, you won't be recieving an e-mail from us. Be on
+            the lookout however, our systems are working 24/7 to constantly
+            check against any profile that is added on our site.
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
