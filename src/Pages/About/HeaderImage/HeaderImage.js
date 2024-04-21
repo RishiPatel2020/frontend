cf69e8fbd48b3f77f31ea6f78be61ac7f712b618
RishiPatel0290 <98559879+RishiPatel2020@users.noreply.github.com>
2024-04-21 02:11:54 -0400

    changes made to abut page

--- src/Pages/About/HeaderImage/HeaderImage.js
+++ src/Pages/About/HeaderImage/HeaderImage.js
@@ -4,10 +4,6 @@ import './HeaderImage.css';  // Ensure the stylesheet is correctly linked
 const HeaderImage = () => {
   return (
     <div className="header-image">
-      {/* <h1 className="header-title">
-        <span className="dream-text text-info">Dream of Love,</span>
-        <span className="reality-text text-info">Awaken to Reality.</span>
-      </h1> */}
     </div>
   );
 }
